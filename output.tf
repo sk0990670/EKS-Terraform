f077c22a70c4637b9305a38361e687023fdcb06a
sujeet Kumar <99119279+sk0990670@users.noreply.github.com>
2025-05-08 17:13:47 +0530

    Update output.tf
    change according to my configuration

--- output.tf
+++ output.tf
@@ -1,16 +1,16 @@
 output "cluster_id" {
-  value = aws_eks_cluster.devopsshack.id
+  value = aws_eks_cluster.solo.id
 }
 
 output "node_group_id" {
-  value = aws_eks_node_group.devopsshack.id
+  value = aws_eks_node_group.solo.id
 }
 
 output "vpc_id" {
-  value = aws_vpc.devopsshack_vpc.id
+  value = aws_vpc.solo_vpc.id
 }
 
 output "subnet_ids" {
-  value = aws_subnet.devopsshack_subnet[*].id
+  value = aws_subnet.solo_subnet[*].id
 }
 
